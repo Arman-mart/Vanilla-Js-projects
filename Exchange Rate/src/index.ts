cc6cac19d3aaabaffda4f0c6e33b7e7bd80d32ed
Arman <arman.m.9a@gmail.com>
2022-06-01 18:58:17 +0400

    .

---
+++ Exchange Rate/src/index.ts
@@ -0,0 +1,9 @@
+const firstCurrency = document.getElementById('currency-one')  as HTMLOptionElement | null;
+const amountOne = document.getElementById('amount-one') as HTMLInputElement | null;
+const secondCurrency = document.getElementById('currency-two')  as HTMLOptionElement | null;
+const amountTwo = document.getElementById('amount-two') as HTMLInputElement | null;
+
+const rate = document.getElementById('rate')  as HTMLDivElement | null;
+const swap = document.getElementById('swap')  as HTMLButtonElement | null;
+
+
